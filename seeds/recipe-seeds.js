2876ba0f714b6b595317184a90f95b36598d0f2b
Anusha <anushar20201@gmail.com>
2022-08-04 20:22:16 -0500

    seeding the data

--- seeds/recipe-seeds.js
+++ seeds/recipe-seeds.js
@@ -2,11 +2,26 @@ const { Post } = require('../models');
 
 const postdata = [
   {
-    
+    recipe_name : 'Oatmeal' ,
+    diet_type : 'Vegan',
+    ingredients : 'Oats, milk',
+    instructions : 'Boil some water and oats together ' ,
+    user_name : 'Kevin'
   },
   {
-
-  }
+    recipe_name : 'Ice',
+    diet_type : 'Gluten-free' ,
+    ingredients : 'Water',
+    instructions : 	'Boil some water, pour it into an ice tray, and place it in freezer', 
+    user_name : 'Ted'
+  },
+  {
+    recipe_name : 'Toast',
+    diet_type : 'Vegetarian' ,
+    ingredients : 'Bread, butter',
+    instructions : 'Apply some butter on pan, and toast the bread on both sides',
+    user_name : 'Mary'
+  },
 ];
 
 const seedPosts = () => Post.bulkCreate(postdata);
--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -3,53 +3,33 @@ const { User, Recipe } = require('../models');
 
 const userdata = [
   {
-    user_name: 'alesmonde0',
-    email: 'nwestnedge0@cbc.ca',
+    user_name: 'sam',
+    email: 'sam@cbc.ca',
     password: 'password123'
   },
   {
-    user_name: 'jwilloughway1',
-    email: 'rmebes1@sogou.com',
+    user_name: 'Bob',
+    email: 'Bob1@sogou.com',
     password: 'password123'
   },
   {
-    user_name: 'iboddam2',
-    email: 'cstoneman2@last.fm',
+    user_name: 'Mark',
+    email: 'Mark2@last.fm',
     password: 'password123'
   },
   {
-    user_name: 'dstanmer3',
-    email: 'ihellier3@goo.ne.jp',
+    user_name: 'Kevin',
+    email: 'Kevin3@goo.ne.jp',
     password: 'password123'
   },
   {
-    user_name: 'djiri4',
-    email: 'gmidgley4@weather.com',
+    user_name: 'Mary',
+    email: 'Mary4@weather.com',
     password: 'password123'
   },
   {
-    user_name: 'msprague5',
-    email: 'larnout5@imdb.com',
-    password: 'password123'
-  },
-  {
-    user_name: 'mpergens6',
-    email: 'hnapleton6@feedburner.com',
-    password: 'password123'
-  },
-  {
-    user_name: 'tpenniell7',
-    email: 'kperigo7@china.com.cn',
-    password: 'password123'
-  },
-  {
-    user_name: 'msabbins8',
-    email: 'lmongain8@google.ru',
-    password: 'password123'
-  },
-  {
-    user_name: 'jmacarthur9',
-    email: 'bsteen9@epa.gov',
+    user_name: 'Ted',
+    email: 'Ted@imdb.com',
     password: 'password123'
   }
 ];
