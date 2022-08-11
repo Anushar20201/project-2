21e44217994511527cfc6dc7cbeae4c95f7dcb05
Shane Dinh <shane.dinh5@gmail.com>
2022-08-10 21:54:42 -0500

    post recipe page linked and able to post new recipes, some other small changes

---
+++ public/javascript/newrecipe.js
@@ -0,0 +1,33 @@
+async function newFormHandler(event) {
+  event.preventDefault();
+
+  const recipe_name = document.querySelector("#recipename").value;
+  const diet_type = document.querySelector("#diet").value;
+  const ingredients = document.querySelector("#ingredients").value;
+  const instructions = document.querySelector("#instructions").value;
+
+  if (recipe_name && diet_type && ingredients && instructions) {
+    const response = await fetch(`/api/recipes`, {
+      method: "POST",
+      body: JSON.stringify({
+        recipe_name,
+        diet_type,
+        ingredients,
+        instructions,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (response.ok) {
+      document.location.replace("/");
+    } else {
+      alert(response.statusText);
+    }
+  }
+}
+
+document
+  .querySelector(".recipe-form")
+  .addEventListener("submit", newFormHandler);
