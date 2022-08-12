1ceb67b298571bc17264b115acba6a4a9150ab6d
Anusha <anushar20201@gmail.com>
2022-08-11 21:17:36 -0500

    adding heroku configs

--- config/connection.js
+++ config/connection.js
@@ -8,6 +8,7 @@ const sequelize = process.env.JAWSDB_URL
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
       host: 'localhost',
       dialect: 'mysql',
+      protocol: 'mysql',
       port: 3306
     });
 
